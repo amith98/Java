1fb0be0677c385487b7c6f598e868d810e6ceb2e
Amith <amithar1998@gmail.com>
2025-09-25 17:32:12 +0530

    School store

---
+++ SchoolStoreAssignment.java
@@ -0,0 +1,51 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class SchoolStoreAssignment {
+    public static void main(String[] args) {
+        String content = "Java File Handling Practice";
+        char array[] = new char[100];
+        // Creating a file
+        File fileObj = new File("assignment.txt");
+        try {
+            boolean value = fileObj.createNewFile();
+            if (value) {
+                System.out.println("File created");
+            } else {
+                System.out.println("File already exists");
+            }
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+        // Writing content to file
+        try {
+            FileWriter fileWriterObj = new FileWriter("assignment.txt");
+            fileWriterObj.write(content);
+            System.out.println("Content is written to file");
+            fileWriterObj.close();
+
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+        // Reading content from the file
+        try {
+            FileReader fileReaderObj = new FileReader("assignment.txt");
+            fileReaderObj.read(array);
+            String readContent = new String(array);
+            System.out.println("Content from file:\n" + readContent);
+            fileReaderObj.close();
+
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+        // Deleting the file
+        boolean value = fileObj.delete();
+        if (value) {
+            System.out.println("File deleted");
+        } else {
+            System.out.println("File not deleted");
+        }
+    }
+
+}
